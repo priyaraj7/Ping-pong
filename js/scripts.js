e748ba3bda84bac90f6cfb6c4a3ad53d96c6e3f1
Priyaraj7 <suppichala@gmail.com>
2020-02-01 20:28:37 -0800

    change the array into list

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,54 @@
+// $(document).ready(function () {
+//     $("form#pingpong").submit(function (event) {
+//         event.preventDefault();
+//         var inputNum = parseInt($("input#multiply").val());
+//         var resultEl = $('#result')//.empty();
+//         for (var i = 1; i <= inputNum; i++) {
+//             var item;
+//             if (i % 15 === 0) {
+//                 item = "ORANGE";
+//             } else if (i % 5 === 0) {
+//                 item = "pong";
+//             } else if (i % 3 === 0) {
+//                 item = "ping";
+//             } else {
+//                 item = i;
+//             }
+//             $('<li>').text(item).appendTo(resultEl);
+//         }
+//     });
+// });
+
+
 $(document).ready(function () {
     $("form#pingpong").submit(function (event) {
         event.preventDefault();
-        var inputNum = parseInt($("input#multiply").val());
-        var result = [];
-
+        var inputStr = $("#multiply").val();
+        if(inputStr === '') {
+            alert('Please enter a number');
+            return;
+        }
+        var inputNum = parseInt(inputStr);
+        if(Number.isNaN(inputNum)) {
+            alert("Please enter a number");
+            return;
+        }
+        if(inputNum <= 0) {
+            alert("Please enter a number > 1");
+            return;
+        }
+        var resultEl = $('#result').empty();
         for (var i = 1; i <= inputNum; i++) {
+            var item;
             if (i % 15 === 0) {
-                result.push("ORANGE");
+                $('<li>').text("Orange").appendTo(resultEl);
             } else if (i % 5 === 0) {
-                result.push("pong");
+                $('<li>').text("pong").appendTo(resultEl);
             } else if (i % 3 === 0) {
-                result.push("ping");
+                $('<li>').text("ping").appendTo(resultEl);
             } else {
-                result.push(" " + i);
+                $('<li>').text(i).appendTo(resultEl);
             }
         }
-
-        $("#result").text(result);
     });
 });
